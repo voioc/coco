3fe7e364ad56224fe090a217c6b5227a39aeed87
见雪松 <jianxuesong3@le.com>
2021-01-26 16:25:16 +0800

    修改部分逻辑

--- cache/Cache.go
+++ cache/Cache.go
@@ -2,7 +2,6 @@ package cache
 
 import (
 	"fmt"
-	// log "lemon/lib/log"
 	"time"
 
 	"github.com/bradfitz/gomemcache/memcache"
@@ -10,11 +9,12 @@ import (
 	jsoniter "github.com/json-iterator/go"
 	GoCache "github.com/patrickmn/go-cache"
 	"github.com/voioc/coco/config"
-	log "github.com/voioc/coco/log"
+	"github.com/voioc/coco/logcus"
 )
 
 type cacheConfigSingle struct {
 	Driver   string   `json:"driver"`
+	Type     string   `json:"type"`
 	Nodes    []string `json:"nodes"`
 	Password string   `json:"password"`
 	Expire   int32    `json:"exire"`
@@ -24,21 +24,32 @@ type cacheConfigSingle struct {
 // Redis 通用Redis类型
 type Redis interface {
 	Do(...interface{}) *redis.Cmd
+
+	Exists(...string) *redis.IntCmd
 	Ping() *redis.StatusCmd
-	Get(key string) *redis.StringCmd
+	Get(string) *redis.StringCmd
+	Del(...string) *redis.IntCmd
+
 	Set(string, interface{}, time.Duration) *redis.StatusCmd
 	ExpireAt(string, time.Time) *redis.BoolCmd
+	Expire(string, time.Duration) *redis.BoolCmd
 	IncrBy(string, int64) *redis.IntCmd
 
-	ZCard(string) *redis.IntCmd
-	ZScan(string, uint64, string, int64) *redis.ScanCmd
-	ZIncrBy(string, float64, string) *redis.FloatCmd
-	ZRevRangeByScoreWithScores(string, redis.ZRangeBy) *redis.ZSliceCmd
+	HGet(string, string) *redis.StringCmd
+	HGetAll(string) *redis.StringStringMapCmd
+	HIncrBy(string, string, int64) *redis.IntCmd
+
+	LLen(string) *redis.IntCmd
+	LIndex(string, int64) *redis.StringCmd
+	RPop(string) *redis.StringCmd
+	LPush(string, ...interface{}) *redis.IntCmd
+
+	SetNX(string, interface{}, time.Duration) *redis.BoolCmd
 }
 
 var cacheConfig []cacheConfigSingle
 
-var redisClient Redis
+var redisClient *redis.ClusterClient
 var cacheClient *GoCache.Cache
 var memClient *memcache.Client
 
@@ -47,6 +58,10 @@ var memClient *memcache.Client
 
 // Init 初始化
 func Init() {
+	conn()
+}
+
+func conn() {
 	config.GetConfig().UnmarshalKey("cache", &cacheConfig)
 
 	for _, cache := range cacheConfig {
@@ -64,13 +79,7 @@ func Init() {
 		}
 
 		if cache.Driver == "redis" {
-			if len(cache.Nodes) == 1 {
-				redisClient = redis.NewClient(&redis.Options{
-					Addr:     cache.Nodes[0],
-					Password: cache.Password, // no password set
-					DB:       0,              // use default DB
-				})
-			} else {
+			if cache.Type == "cluster" {
 				redisClient = redis.NewClusterClient(&redis.ClusterOptions{
 					Addrs:    cache.Nodes,
 					Password: cache.Password,
@@ -78,23 +87,25 @@ func Init() {
 			}
 
 			if ping, err := redisClient.Ping().Result(); err != nil {
-				log.Print("info", "Test Redis Server:"+ping+"the error is: "+err.Error())
+				logcus.Print("info", "Test Redis Server:"+ping+"the error is: "+err.Error())
 			}
 		}
 	}
 }
 
 // GetRedis 获得redis
-func GetRedis() Redis {
+func GetRedis() *redis.ClusterClient {
+	if redisClient == nil {
+		conn()
+	}
+
 	return redisClient
 }
 
 // GetCache 获取缓存
 func GetCache(cacheKey string, data interface{}) (bool, error) {
-
 	isGet, cacheErr := getCacheByDriver(cacheKey, cacheConfig[0].Driver, data)
-
-	if isGet == false || cacheErr != nil {
+	if (isGet == false || cacheErr != nil) && len(cacheConfig) > 1 {
 		isGet, cacheErr = getCacheByDriver(cacheKey, cacheConfig[1].Driver, data)
 
 		// 从二级缓存拿到数据的话写入一级缓存
@@ -117,8 +128,9 @@ func getCacheByDriver(cacheKey, driver string, dataStruct interface{}) (bool, er
 	case "memcached":
 		valueTmp, err := memClient.Get(cacheKey)
 		if err != nil {
-			log.Print("info", "Cache Memcached get cache, the key is: "+cacheKey+" the error is: "+err.Error())
-			return CacheGet, fmt.Errorf("[info] Cache Memcached get cache: %s", err.Error())
+			info := fmt.Sprintf("Cache Memcached error | {key} %s {error} %s ", cacheKey, err.Error())
+			logcus.Print("info", info)
+			return CacheGet, fmt.Errorf(info)
 		}
 
 		if len(valueTmp.Value) == 0 {
@@ -128,34 +140,36 @@ func getCacheByDriver(cacheKey, driver string, dataStruct interface{}) (bool, er
 
 	case "redis":
 		if value, err = redisClient.Do("GET", cacheKey).String(); err != nil {
-			log.Print("info", "Cache there is error when get value from key: "+cacheKey+" error is: "+err.Error())
-			return false, fmt.Errorf("[info] Cache there is error when get value from key %s : %s", cacheKey, err.Error())
+			info := fmt.Sprintf("Cache Reids error | {key} %s {error} %s ", cacheKey, err.Error())
+			logcus.Print("info", info)
+			return false, fmt.Errorf(info)
 		}
 
-		if err != nil {
-			log.Print("info", "Cache there is error when get value from key: "+cacheKey+" error is: "+err.Error())
-			return false, fmt.Errorf("[info] Cache there is error when get value from key %s : %s", cacheKey, err.Error())
+		// if err != nil {
+		// 	logcus.Print("info", "[Cache] there is error when get value from key: "+cacheKey+" error is: "+err.Error())
+		// 	return false, fmt.Errorf("[info] Cache there is error when get value from key %s : %s", cacheKey, err.Error())
+		// }
+	}
+
+	if value != "" {
+		if err := json.UnmarshalFromString(value, dataStruct); err != nil {
+			info := fmt.Sprintf("Cache Format error | {key} %s {value} %s {error} %s ", cacheKey, value, err.Error())
+			logcus.Print("info", info)
+			return false, fmt.Errorf(info)
 		}
 	}
 
-	if value == "" {
-		return false, nil
-	}
+	// CacheGet = true
 
-	if err := json.UnmarshalFromString(value, dataStruct); err != nil {
-		log.Print("info", "Cache get cache the key is: "+cacheKey+" the value is: "+value+" the error is: "+err.Error())
-		return false, fmt.Errorf("[info]LCache Redisc get cache: %s", err.Error())
-	}
-
-	CacheGet = true
-
-	return CacheGet, nil
+	return true, nil
 }
 
 // SetCache 写入缓存
 func SetCache(cacheKey string, data interface{}, expire int32) error {
 	setCacheByDriver(cacheKey, cacheConfig[0].Driver, data, expire)
-	setCacheByDriver(cacheKey, cacheConfig[1].Driver, data, expire)
+	if len(cacheConfig) > 1 {
+		setCacheByDriver(cacheKey, cacheConfig[1].Driver, data, expire)
+	}
 
 	return nil
 }
@@ -166,12 +180,16 @@ func setCacheByDriver(cacheKey, driver string, data interface{}, expire int32) e
 
 	dataStr, err := json.MarshalToString(data)
 	if err != nil {
-		log.Print("info", "Cache marshall struct: "+err.Error())
+		logcus.Print("info", "Cache marshall struct: "+err.Error())
 		return fmt.Errorf("[info] Cache marshall struct: %s", err.Error())
 	}
 
 	switch driver {
 	case "memcached":
+		if memClient == nil {
+			conn()
+		}
+
 		item := &memcache.Item{
 			Key:        cacheKey,
 			Value:      []byte(dataStr),
@@ -180,15 +198,24 @@ func setCacheByDriver(cacheKey, driver string, data interface{}, expire int32) e
 
 		err := memClient.Set(item)
 		if err != nil {
-			log.Print("info", "Cache Memcached set cache: "+err.Error())
+			logcus.Print("info", "Cache Memcached set cache: "+err.Error())
 			return fmt.Errorf("[info]Cache Memcached set cache: %s", err.Error())
 		}
 
 	case "redis":
-		errCmd := redisClient.Do("SET", cacheKey, dataStr, "EX", expire)
-		if errCmd.Err() != nil {
-			log.Print("info", "Cache Redis set cache:: "+errCmd.Err().Error())
-			return fmt.Errorf("[info] Cache Redis set cache: %s", errCmd.Err().Error())
+		if redisClient == nil {
+			conn()
+		}
+
+		if expire == -1 {
+			err = redisClient.Do("SET", cacheKey, dataStr).Err()
+		} else {
+			err = redisClient.Do("SET", cacheKey, dataStr, "EX", expire).Err()
+		}
+
+		if err != nil {
+			logcus.Print("info", "Cache Redis set cache:: "+err.Error())
+			return fmt.Errorf("[info] Cache Redis set cache: %s", err.Error())
 		}
 	}
 
