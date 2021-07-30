3e2f10c02f705fa838aa4432975ab776284cc554
见雪松 <jianxuesong@tal.com>
2021-07-30 19:17:01 +0800

    zap 日志

--- main.go
+++ main.go
@@ -9,34 +9,87 @@
 package main
 
 import (
-	"github.com/voioc/coco/logcus"
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+const (
+	logTmFmtWithMS = "2006-01-02 15:04:05.000"
 )
 
 func main() {
-	// log := logrus.New()
-	// logrus.SetReportCaller(true)
-	// logrus.SetLevel(logrus.TraceLevel)
-	// logrus.SetFormatter(&lf.Formatter{
-	// 	HideKeys:        true,
-	// 	TimestampFormat: "[2006/01/02 15:04:05]",
-	// 	// FieldsOrder:     []string{"name", "age"},
-	// 	ShowFullLevel: true,
-	// })
 
-	// stdout := os.Stdout
-	// file, err := os.OpenFile("/tmp/error.log", os.O_WRONLY|os.O_CREATE, 0755)
-	// if err != nil {
-	// 	log.Fatalf("create file log.txt failed: %v", err)
-	// }
-
-	// logrus.SetOutput(io.MultiWriter(file, stdout))
-
-	// logrus.WithField("component", "main").Error("sdf")
-	logcus.Info("abcdejf")
+	core := initCore()
+	logger := zap.New(core, zap.AddCaller())
+	logger.Error("Error Info:", zap.String("url", "www.baidu.com"))
 	// logrus.Trace("trace msg")
 	// logrus.Debug("debug msg")
 	// logrus.Info("info msg")
 	// logrus.Warn("warn msg")
 	// logrus.Error("error msg")
 
+}
+
+func initCore() zapcore.Core {
+	opts := []zapcore.WriteSyncer{
+		zapcore.AddSync(&lumberjack.Logger{
+			Filename:  "test.log", // ⽇志⽂件路径
+			MaxSize:   512,        // 单位为MB,默认为512MB
+			MaxAge:    7,          // 文件最多保存多少天
+			LocalTime: true,       // 采用本地时间
+			Compress:  false,      // 是否压缩日志
+		}),
+	}
+
+	// if l.stdout {
+	// 	opts = append(opts, zapcore.AddSync(os.Stdout))
+	// }
+
+	syncWriter := zapcore.NewMultiWriteSyncer(opts...)
+
+	// 自定义时间输出格式
+	customTimeEncoder := func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString("[" + t.Format(logTmFmtWithMS) + "]")
+	}
+	// 自定义日志级别显示
+	customLevelEncoder := func(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString("[" + level.CapitalString() + "]")
+	}
+
+	// 自定义文件：行号输出项
+	customCallerEncoder := func(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayEncoder) {
+		// enc.AppendString("[" + l.traceId + "]")
+		enc.AppendString("[" + caller.TrimmedPath() + "]")
+	}
+
+	encoderConf := zapcore.EncoderConfig{
+		CallerKey:      "caller_line", // 打印文件名和行数
+		LevelKey:       "level_name",
+		MessageKey:     "msg",
+		TimeKey:        "ts",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeTime:     customTimeEncoder,   // 自定义时间格式
+		EncodeLevel:    customLevelEncoder,  // 小写编码器
+		EncodeCaller:   customCallerEncoder, // 全路径编码器
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeName:     zapcore.FullNameEncoder,
+	}
+
+	// // level大写染色编码器
+	// if l.enableColor {
+	// 	encoderConf.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	// }
+
+	// // json 格式化处理
+	// if l.jsonFormat {
+	// 	return zapcore.NewCore(zapcore.NewJSONEncoder(encoderConf),
+	// 		syncWriter, zap.NewAtomicLevelAt(l.logMinLevel))
+	// }
+
+	return zapcore.NewCore(zapcore.NewConsoleEncoder(encoderConf),
+		syncWriter, zap.NewAtomicLevelAt(zapcore.DebugLevel))
 }
