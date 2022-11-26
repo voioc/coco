7012f1304ea4c566751b2e5e2de6d48e55896370
Jxs <jianxuesong@xiwang.com>
2022-11-26 19:53:49 +0800

    日志库

--- logzap/zap.go
+++ logzap/zap.go
@@ -1,11 +1,10 @@
-package logzap
+package logger
 
 import (
 	"context"
 	"fmt"
 	"os"
 	"strings"
-	"time"
 
 	jsoniter "github.com/json-iterator/go"
 	"github.com/spf13/viper"
@@ -14,56 +13,86 @@ import (
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
-// var logzap = zap.New(initCore(), zap.AddCallerSkip(1), zap.AddStacktrace(zapcore.WarnLevel), zap.AddCaller())
-var logzap *zap.Logger
+var logZap *zap.Logger
 
 type ContextKey string
 
-const (
-	logTmFmtWithMS = "2006-01-02 15:04:05.000"
-)
+type Logger struct {
+	Filename   string // ⽇志⽂件路径
+	MaxSize    int    // 单位为MB,默认为100MB
+	MaxAge     int    // 文件最多保存多少天
+	MaxBackups int
+}
+
+var logConf *Logger
 
 func Zap() *zap.Logger {
-	return logzap
+	return logZap
 }
 
-func InitZap() {
-	logzap = zap.New(initCore(), zap.AddCallerSkip(1), zap.AddCaller())
+func InitZap(name string) {
+	if logConf == nil {
+		logConf = &Logger{}
+	}
+
+	if logConf.Filename == "" {
+		logPath := "/log/"
+		env := strings.ToLower(os.Getenv("envType"))
+		if env != "product" && env != "test" && env != "gray" {
+			logPath = "runtime/"
+		}
+
+		logPath = fmt.Sprintf("%s%s/%s.log", logPath, name, name)
+
+		if closed := viper.GetBool("log.closed"); closed {
+			logPath = "/dev/null"
+		}
+
+		logConf.Filename = logPath
+	}
+
+	if logConf.MaxSize == 0 {
+		maxSize := viper.GetInt("log.max_size")
+		if maxSize == 0 {
+			maxSize = 5120
+		}
+
+		logConf.MaxSize = maxSize
+	}
+
+	if logConf.MaxAge == 0 {
+		maxAge := viper.GetInt("log.max_age")
+		if maxAge == 0 {
+			maxAge = 7
+		}
+		logConf.MaxAge = maxAge
+	}
+
+	if logConf.MaxBackups == 0 {
+		maxBackup := viper.GetInt("log.max_backup")
+		if maxBackup == 0 { // 如果未设置则默认保留5个
+			maxBackup = 5
+		}
+
+		if maxBackup < 0 { // 如果设置为 -1 代表保留全部
+			maxBackup = 0
+		}
+
+		logConf.MaxBackups = maxBackup
+	}
+
+	logZap = zap.New(initCore(logConf), zap.AddCallerSkip(1), zap.AddCaller())
 }
 
-func initCore() zapcore.Core {
-	logPath := "runtime/app.log"
-	if errlog := viper.GetString("log.error"); errlog != "" {
-		logPath = errlog
-	}
-
-	if closed := viper.GetBool("log.closed"); closed {
-		logPath = "/dev/null"
-	}
-
-	maxSize := viper.GetInt("log.max_size")
-	if maxSize == 0 {
-		maxSize = 5120
-	}
-
-	maxAge := viper.GetInt("log.max_age")
-	if maxSize == 0 {
-		maxAge = 7
-	}
-
-	maxBackup := viper.GetInt("log.max_backup")
-	if maxBackup == 0 {
-		maxBackup = 5
-	}
-
+func initCore(config *Logger) zapcore.Core {
 	opts := []zapcore.WriteSyncer{
 		zapcore.AddSync(&lumberjack.Logger{
-			Filename:   logPath, // fmt.Sprintf("%s%s/%s.log", logPath, name, name), // ⽇志⽂件路径
-			MaxSize:    maxSize, // 单位为MB,默认为100MB
-			MaxAge:     maxAge,  // 文件最多保存多少天
-			MaxBackups: maxBackup,
-			LocalTime:  true,  // 采用本地时间
-			Compress:   false, // 是否压缩日志
+			Filename:   config.Filename, // ⽇志⽂件路径
+			MaxSize:    config.MaxSize,  // 单位为MB,默认为100MB
+			MaxAge:     config.MaxAge,   // 文件最多保存多少天
+			LocalTime:  true,            // 采用本地时间
+			Compress:   false,           // 是否压缩日志
+			MaxBackups: config.MaxBackups,
 		}),
 	}
 
@@ -71,16 +100,13 @@ func initCore() zapcore.Core {
 	// 	opts = append(opts, zapcore.AddSync(os.Stdout))
 	// }
 
-	if env := strings.ToLower(os.Getenv("RunEnv")); env == "debug" {
-		opts = append(opts, zapcore.AddSync(os.Stdout))
-	}
-
 	syncWriter := zapcore.NewMultiWriteSyncer(opts...)
 
 	// 自定义时间输出格式
-	customTimeEncoder := func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
-		enc.AppendString("" + t.Format(logTmFmtWithMS) + "")
-	}
+	// customTimeEncoder := func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	// 	enc.AppendString(t.Format(logTmFmtWithMS))
+	// 	// enc.AppendString(strconv.Itoa(int(t.UnixNano() / 1e6)))
+	// }
 
 	// 自定义日志级别显示
 	customLevelEncoder := func(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
@@ -94,16 +120,16 @@ func initCore() zapcore.Core {
 	}
 
 	encoderConf := zapcore.EncoderConfig{
-		CallerKey:      "caller", // 打印文件名和行数
+		CallerKey:      "caller_line", // 打印文件名和行数
 		LevelKey:       "level",
 		MessageKey:     "msg",
-		TimeKey:        "time",
+		TimeKey:        "@timestamp",
 		StacktraceKey:  "stacktrace",
 		LineEnding:     zapcore.DefaultLineEnding,
-		EncodeTime:     customTimeEncoder,   // 自定义时间格式
-		EncodeLevel:    customLevelEncoder,  // 小写编码器
-		EncodeCaller:   customCallerEncoder, // 全路径编码器
-		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeTime:     zapcore.RFC3339TimeEncoder, // 自定义时间格式
+		EncodeLevel:    customLevelEncoder,         // 小写编码器
+		EncodeCaller:   customCallerEncoder,        // 全路径编码器
+		EncodeDuration: zapcore.MillisDurationEncoder,
 		EncodeName:     zapcore.FullNameEncoder,
 	}
 
@@ -133,23 +159,19 @@ func formatField(c context.Context, tag string) []zapcore.Field {
 		return fields
 	}
 
-	// if g, ok := c.(*gin.Context); ok {
-	// 	c = g.Request.Context()
-	// }
-
-	var traceID string
-	trace := c.Value(ContextKey("x_trace_id"))
-	if id, ok := trace.(string); ok {
-		traceID = id
+	requestURI := c.Value(ContextKey("request_uri"))
+	if uri, ok := requestURI.(string); ok && uri != "" {
+		fields = append(fields, zap.String("request_uri", uri))
 	}
 
-	// if traceID == "" {
-	// 	traceID = uuid.NewV4().String()
-	// }
+	// var traceID string
+	trace := c.Value(ContextKey("x_trace_id"))
+	if traceID, ok := trace.(string); ok {
+		fields = append(fields, zap.String("x_trace_id", traceID))
+	}
 
-	return append(fields, zap.String("x_trace_id", traceID))
+	return fields
 }
-
 func Ix(c context.Context, tag string, template interface{}, args ...interface{}) {
 	var msg string
 
@@ -162,7 +184,7 @@ func Ix(c context.Context, tag string, template interface{}, args ...interface{}
 	}
 
 	fields := formatField(c, tag)
-	logzap.Info(msg, fields...)
+	logZap.Info(msg, fields...)
 }
 
 func Ex(c context.Context, tag string, template interface{}, args ...interface{}) {
@@ -176,7 +198,7 @@ func Ex(c context.Context, tag string, template interface{}, args ...interface{}
 	}
 
 	fields := formatField(c, tag)
-	logzap.Error(msg, fields...)
+	logZap.Error(msg, fields...)
 }
 
 func Dx(c context.Context, tag string, template interface{}, args ...interface{}) {
@@ -190,7 +212,7 @@ func Dx(c context.Context, tag string, template interface{}, args ...interface{}
 	}
 
 	fields := formatField(c, tag)
-	logzap.Debug(msg, fields...)
+	logZap.Debug(msg, fields...)
 }
 
 func Wx(c context.Context, tag string, template interface{}, args ...interface{}) {
@@ -204,7 +226,7 @@ func Wx(c context.Context, tag string, template interface{}, args ...interface{}
 	}
 
 	fields := formatField(c, tag)
-	logzap.Warn(msg, fields...)
+	logZap.Warn(msg, fields...)
 }
 
 func DPx(c context.Context, tag string, template interface{}, args ...interface{}) {
@@ -218,7 +240,7 @@ func DPx(c context.Context, tag string, template interface{}, args ...interface{
 	}
 
 	fields := formatField(c, tag)
-	logzap.DPanic(msg, fields...)
+	logZap.DPanic(msg, fields...)
 }
 
 func Px(c context.Context, tag string, template interface{}, args ...interface{}) {
@@ -232,7 +254,7 @@ func Px(c context.Context, tag string, template interface{}, args ...interface{}
 	}
 
 	fields := formatField(c, tag)
-	logzap.Panic(msg, fields...)
+	logZap.Panic(msg, fields...)
 }
 
 func Fx(c context.Context, tag string, template interface{}, args ...interface{}) {
@@ -246,5 +268,5 @@ func Fx(c context.Context, tag string, template interface{}, args ...interface{}
 	}
 
 	fields := formatField(c, tag)
-	logzap.Fatal(msg, fields...)
+	logZap.Fatal(msg, fields...)
 }
